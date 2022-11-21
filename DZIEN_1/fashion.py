18188514be77ede43583917d0fc5877a472cfd0b
albim72 <marcin.albiniak72@gmail.com>
2022-11-21 15:40:23 +0100

    Update fashion.py

--- DZIEN_1/fashion.py
+++ DZIEN_1/fashion.py
@@ -6,3 +6,6 @@ fashion_mnist = tf.keras.datasets.fashion_mnist
 (train_images,train_labels),(test_images,test_labels) = fashion_mnist.load_data()
 
 class_names = ['T-shirt/top','Trouser','Pullover','Dress','Coat','Sandal','Shirt','Sneaker','Bag','Ankle boot']
+
+train_images.shape
+train_labels
