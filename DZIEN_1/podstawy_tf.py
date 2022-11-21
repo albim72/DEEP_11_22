a35c5709824b9e59840ed43211127e19cbf4569e
albim72 <marcin.albiniak72@gmail.com>
2022-11-21 14:48:47 +0100

    Update podstawy_tf.py

--- DZIEN_1/podstawy_tf.py
+++ DZIEN_1/podstawy_tf.py
@@ -1,2 +1,8 @@
 import tensorflow as tf
 print(f"wersja tf: {tf.__version__}")
+
+#ładowanie zbioru danych zewnętrznych -> mnist -> obrazki 28x28
+mnist = tf.keras.datasets.mnist
+(x_train,y_train),(x_test,y_test) = mnist.load_data()
+#normalizacja danych
+x_train,x_test = x_train/255.0, x_test/255.0
