437598d9b606ac21fcb6ad9483027a468b922355
albim72 <marcin.albiniak72@gmail.com>
2022-11-21 11:16:35 +0100

    Update osoba.py

--- DZIEN_1/osoba.py
+++ DZIEN_1/osoba.py
@@ -33,3 +33,23 @@ dt = 3
 print(f"wiek osoby za {dt} wynosi: {os2.wiekzaxlat(dt)} lat")
 
 
+class Pacjent(Osoba):
+    def __init__(self,imie,wiek,waga,wzrost,nr_pacjenta,placowka,rodzaj_wizyty):
+        super().__init__(imie,wiek,waga,wzrost)
+        self.rodzaj_wizyty = rodzaj_wizyty
+        self.placowka = placowka
+        self.nr_pacjenta = nr_pacjenta
+
+
+print("***************************")
+
+pc1 = Pacjent("Cyryl",23,88,173,54545,"NZOZ Badacz","wizyta kontrolna")
+pc1.dane_osoby()
+wk = 20
+print(f"wiek osoby za {wk} wynosi: {os2.wiekzaxlat(wk)} lat")
+
+
+
+
+
+
