387501ce5e863a2bb65e4082effa5b2d165656fc
albim72 <marcin.albiniak72@gmail.com>
2022-11-21 15:08:20 +0100

    Update podstawy_tf.py

--- DZIEN_1/podstawy_tf.py
+++ DZIEN_1/podstawy_tf.py
@@ -23,3 +23,7 @@ predictions
 
 loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 loss_fn(y_train[:1],predictions).numpy()
+
+model.compile(optimizer='adam',
+              loss=loss_fn,
+              metrics=['accuracy'])
