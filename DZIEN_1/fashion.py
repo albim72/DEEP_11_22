00dfb32acbdfc55eef2bc4c42f4a60e99eae8645
albim72 <marcin.albiniak72@gmail.com>
2022-11-22 09:19:05 +0100

    Update fashion.py

--- DZIEN_1/fashion.py
+++ DZIEN_1/fashion.py
@@ -52,3 +52,22 @@ predictions[0]
 np.argmax(predictions[0])
 
 
+def plot_image(i,predictions_array,true_label,img):
+    true_label,img = true_label[i],img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(img,cmap=plt.cm.binary)
+    predicted_label = np.argmax(predictions_array)
+    if predictions_array == true_label:
+        color = 'blue'
+    else:
+        color='red'
+    plt.xlabel("{} {:2.0f}% ({})".format(
+        class_names[predicted_label],
+        100*np.max(predicted_label),
+        class_names[true_label]
+    ),color=color)
+    
+    
+  
