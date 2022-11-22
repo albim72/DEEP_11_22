dd8775f296541d4963f36f1809c9ad54f43ebefe
albim72 <marcin.albiniak72@gmail.com>
2022-11-22 13:47:20 +0100

    Update konwolucja.py

--- DZIEN_2/konwolucja.py
+++ DZIEN_2/konwolucja.py
@@ -1,3 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras import datasets,layers,models
 import matplotlib.pyplot as plt
+
+#przygotowanie dataset -> CIFAR10
+(train_images,train_labels),(test_images,test_labels) = datasets.cifar10.load_data()
+train_images,test_images = train_images/255.0, test_images/255.0
