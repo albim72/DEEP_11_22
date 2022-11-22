952f70a8aa40611ce7606ff99492505a90d76251
albim72 <marcin.albiniak72@gmail.com>
2022-11-22 09:01:02 +0100

    Update fashion.py

--- DZIEN_1/fashion.py
+++ DZIEN_1/fashion.py
@@ -42,4 +42,9 @@ model.fit(train_images,train_labels,epochs=10)
 test_loss,test_acc = model.evaluate(test_images,test_labels,verbose=2)
 print(f"Test dokładności: {test_acc}")
 
+probability_model = tf.keras.Sequential([
+    model,
+    tf.keras.layers.Softmax()
+])
+
 
