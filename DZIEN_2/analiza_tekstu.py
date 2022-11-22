84019aa19cc29673324d4e05cadd6fe2faefbe0f
albim72 <marcin.albiniak72@gmail.com>
2022-11-22 15:21:26 +0100

    Update analiza_tekstu.py

--- DZIEN_2/analiza_tekstu.py
+++ DZIEN_2/analiza_tekstu.py
@@ -15,3 +15,44 @@ dataset = tf.keras.utils.get_file("aclImdb_v1",url,untar=True,cache_dir='.',
 
 dataset_dir = os.path.join(os.path.dirname(dataset),'aclImdb')
 
+os.listdir(dataset_dir)
+
+train_dir = os.path.join(dataset_dir,'train')
+os.listdir(train_dir)
+
+sample_file = os.path.join(train_dir,'pos/1181_9.txt')
+with open(sample_file) as f:
+    print(f.read())
+    
+#ładowanie zbioru danych
+remove_dir = os.path.join(train_dir,'unsup')
+shutil.rmtree(remove_dir)
+batch_size = 32
+seed = 42
+raw_train_ds = tf.keras.utils.text_dataset_from_directory(
+    'aclImdb/train',
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='training',
+    seed=seed
+
+)
+for text_batch, label_batch in raw_train_ds.take(1):
+  for i in range(3):
+    print(f"recenzja: {text_batch.numpy()[i]}")
+    print(f"etykieta: {label_batch.numpy()[i]}")
+print(f"etykieta 0 to recenzja: {raw_train_ds.class_names[0]}")
+print(f"etykieta 1 to recenzja: {raw_train_ds.class_names[1]}")
+
+#dane walidacyjne i testowe
+raw_val_ds = tf.keras.utils.text_dataset_from_directory(
+    'aclImdb/train',
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='validation',
+    seed=seed
+
+)
+
+
+
