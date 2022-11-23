0f469c5a12e70cc653b2a3f7c8a641b78a7d115a
albim72 <marcin.albiniak72@gmail.com>
2022-11-23 11:05:28 +0100

    Update regresja.py

--- DZIEN_3/regresja.py
+++ DZIEN_3/regresja.py
@@ -19,6 +19,15 @@ raw_dataset = pd.read_csv('auto-mpg.csv',na_values='?',comment='\t',sep=',',skip
 dataset = raw_dataset.copy()
 dataset.tail()
 
+
+dataset.isna().sum()
+
+dataset = dataset.dropna()
+
+dataset['origin'] = dataset['origin'].map({1:'USA',2:'Europa',3:'Japonia'})
+dataset = pd.get_dummies(dataset,columns=['origin'],prefix='',prefix_sep='')
+dataset.tail()
+
 train_dataset = dataset.sample(frac=0.8, random_state=0)
 test_dataset = dataset.drop(train_dataset.index)
 
@@ -32,3 +41,9 @@ test_features = test_dataset.copy()
 
 train_labels = train_features.pop('mpg')
 test_labels = test_features.pop('mpg')
+
+train_dataset.describe().transpose()[['mean','std']]
+
+normalizer = tf.keras.layers.Normalization(axis = -1)
+
+normalizer.adapt(np.array(train_features))
