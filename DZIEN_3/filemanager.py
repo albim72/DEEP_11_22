d5d245ff4b1c001b54db058f930b79ab299b24c9
albim72 <marcin.albiniak72@gmail.com>
2022-11-23 09:34:47 +0100

    Create filemanager.py

---
+++ DZIEN_3/filemanager.py
@@ -0,0 +1,22 @@
+class FileManager:
+
+    def __init__(self,filename,mode,encd):
+        self.filename = filename
+        self.mode = mode
+        self.encd = encd
+        self.file = None
+
+    def __enter__(self):
+        self.file = open(self.filename, self.mode, encoding=self.encd)
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file.close()
+
+with FileManager('test.txt','a','utf-8') as f:
+    f.write('to jest plik testowy!!!\n')
+
+print(f.closed)
+
+with FileManager('test.txt','r','utf-8') as f:
+    print(f.read())
